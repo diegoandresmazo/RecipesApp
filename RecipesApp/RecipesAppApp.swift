038efe3142be9c77a3dca37703c454518452203b
MAZOD002 <diego.x.mazo.-nd@disney.com>
2022-10-31 16:53:47 -0500

    Initial Commit

---
+++ RecipesApp/RecipesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesAppApp.swift
+//  RecipesApp
+//
+//  Created by Diego Andres Mazo Munoz on 31/10/22.
+//
+
+import SwiftUI
+
+@main
+struct RecipesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
